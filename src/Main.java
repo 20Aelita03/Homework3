5eaf6993e6459a98b7b36061b08ca855250e9349
aires <airesheva@mail.ru>
2022-08-08 00:59:56 +0300

    Сделано 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
 public class Main {
     public static void main(String[] args) {
         //Task 1
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
 
+        //Task 2
+        int clientDeviceYea = 2014;
+        if (clientOS == 0) {
+            if (clientDeviceYea < 2015){
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else if (clientDeviceYea < 2015){
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        //Task 3
+        int year = 2022;
+        if (year / 400 == 0 || (year / 4 == 0 && year / 100 != 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
+        //Task 4
+        }
     }
-}
